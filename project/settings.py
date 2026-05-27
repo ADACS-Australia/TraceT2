e90d8dc7b9ea06e4ab703bc0c75b0b4d7291b897
Torrance Hodgson <torrance@pravic.xyz>
2026-05-27 14:01:47 +0800

    Use STORAGES dictionary in settings.php instead of the deprecated STATICFILES_STORAGE parameter

--- project/settings.py
+++ project/settings.py
@@ -156,9 +156,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 STATICFILES_DIRS = [BASE_DIR / "tracet/static"]
 STATIC_ROOT = BASE_DIR / "static"
+
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+    }
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
